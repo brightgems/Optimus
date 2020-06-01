7bf1bdce1289e783e2095f73093d06d5ddacf8fe
unknown <brightgems@live.cn>
2020-06-02 07:03:39 +0800

    go

--- code/scripts/scripts_local/run_lm_ae_bert_gpt.sh
+++ code/scripts/scripts_local/run_lm_ae_bert_gpt.sh
@@ -1,14 +1,19 @@
 
-export TRAIN_FILE=../data/datasets/wikitext-2/train.txt
-export TEST_FILE=../data/datasets/wikitext-2/valid.txt
+export TRAIN_FILE=../data/datasets/idea/train.txt
+export TEST_FILE=../data/datasets/idea/valid.txt
+
+export GPT2Model_PATH=/d/models/gpt2-essays
+export XLNetModel_PATH=/d/models/xlnet/chinese_xlnet_base_pytorch
+export BERTModel_PATH=/d/models/bert/chinese_wwm_ext_pytorch
+
 export GPU_ID=0,1,2,3,4,5,6,7
 
 CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_ae_pretraining.py \
     --output_dir=../output/local_lmae_wiki2_bert_gpt \
     --encoder_model_type=bert \
-    --encoder_model_name_or_path=bert-base-uncased \
-    --decoder_model_type=gpt2 \
-    --decoder_model_name_or_path=gpt2 \
+    --encoder_model_name_or_path=$BERTModel_PATH \
+    --decoder_model_type=xlnet \
+    --decoder_model_name_or_path=$XLNetModel_PATH \
     --do_train \
     --train_data_file=$TRAIN_FILE \
     --do_eval \
@@ -17,4 +22,5 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_ae_pretraining.py \
     --save_steps 200 \
     --logging_steps 100 \
     --overwrite_output_dir \
+    --block_size 128 \
     --per_gpu_train_batch_size=3
