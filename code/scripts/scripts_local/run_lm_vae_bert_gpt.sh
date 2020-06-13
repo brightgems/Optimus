010652579756f89bd26d12fc32aee0f196a37d20
unknown <brightgems@live.cn>
2020-06-13 10:47:49 +0800

    go

--- code/scripts/scripts_local/run_lm_vae_bert_gpt.sh
+++ code/scripts/scripts_local/run_lm_vae_bert_gpt.sh
@@ -1,5 +1,37 @@
 export PYTHONPATH="${PYTHONPATH}:/workspace/code"
 export GPU_ID=0,1
+
+
+export GPT2Model_PATH=/pretrain-models/gpt2-cn/gpt2-essay
+export BERTModel_PATH=/pretrain-models/bert/bert_torch_base_cn
+
+export TRAIN_FILE=/code/notebook/idea-gen/text_corpus/train.txt
+export TEST_FILE=/code/notebook/idea-gen/text_corpus/test.txt
+export GPU_ID=0,1
+
+
+CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_vae_training.py \
+    --dataset Debug \
+    --output_dir=../output/local_lm_vae_debug_bert_gpt \
+    --encoder_model_type=bert \
+    --encoder_model_name_or_path=$BERTModel_PATH \
+    --decoder_model_type=gpt2 \
+    --decoder_model_name_or_path=$GPT2Model_PATH \
+    --train_data_file=$TRAIN_FILE \
+    --do_train \
+    --do_eval \
+    --beta 1.0 \
+    --ratio_zero .5 \
+    --ratio_increase 0.25 \
+    --eval_data_file=$TEST_FILE \
+    --num_train_epochs 5.0 \
+    --save_steps 20 \
+    --logging_steps 4 \
+    --overwrite_output_dir \
+    --per_gpu_train_batch_size 3 \
+    --block_size 50
+
+
 # export TRAIN_FILE=../data/datasets/wikitext-2/train.txt
 # export TEST_FILE=../data/datasets/wikitext-2/valid.txt
 
