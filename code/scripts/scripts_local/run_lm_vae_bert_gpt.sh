d4e200420e1f39fa785003e70160938f80156894
unknown <brightgems@live.cn>
2020-06-14 18:44:50 +0800

    go

--- code/scripts/scripts_local/run_lm_vae_bert_gpt.sh
+++ code/scripts/scripts_local/run_lm_vae_bert_gpt.sh
@@ -1,37 +1,6 @@
 export PYTHONPATH="${PYTHONPATH}:/workspace/code"
 export GPU_ID=0,1
 
-
-export GPT2Model_PATH=/pretrain-models/gpt2-cn/gpt2-essay
-export BERTModel_PATH=/pretrain-models/bert/bert_torch_base_cn
-
-export TRAIN_FILE=/code/notebook/idea-gen/text_corpus/train.txt
-export TEST_FILE=/code/notebook/idea-gen/text_corpus/valid.txt
-export GPU_ID=0,1
-
-
-CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_vae_training.py \
-    --dataset sgnews \
-    --output_dir=../output/local_lm_vae_sgnews_bert_gpt \
-    --encoder_model_type=bert \
-    --encoder_model_name_or_path=$BERTModel_PATH \
-    --decoder_model_type=gpt2 \
-    --decoder_model_name_or_path=$GPT2Model_PATH \
-    --train_data_file=$TRAIN_FILE \
-    --do_train \
-    --do_eval \
-    --beta 1.0 \
-    --ratio_zero .5 \
-    --ratio_increase 0.25 \
-    --eval_data_file=$TEST_FILE \
-    --num_train_epochs 5.0 \
-    --save_steps 2000 \
-    --logging_steps 100 \
-    --overwrite_output_dir \
-    --per_gpu_train_batch_size 3 \
-    --block_size 50
-
-
 # export TRAIN_FILE=../data/datasets/wikitext-2/train.txt
 # export TEST_FILE=../data/datasets/wikitext-2/valid.txt
 
@@ -238,29 +207,29 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_vae_training.py \
 #     --save_bert_gpt_init
 
 
-export TRAIN_FILE=../data/datasets/glue_data/train.txt
-export TEST_FILE=../data/datasets/glue_data/train.txt
+# export TRAIN_FILE=../data/datasets/glue_data/train.txt
+# export TEST_FILE=../data/datasets/glue_data/train.txt
 
-CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_vae_training.py \
-    --dataset Glue \
-    --checkpoint_dir ../output/philly_scl_b16_g8_vae_wikipedia_pretraining_b0.0_d1.0_r01.0_ra0.1_200 \
-    --gloabl_step_eval 880000 \
-    --output_dir=../output/local_lm_vae_glue_bert_gpt \
-    --encoder_model_type=bert \
-    --encoder_model_name_or_path=bert-base-cased \
-    --decoder_model_type=gpt2 \
-    --decoder_model_name_or_path=gpt2 \
-    --train_data_file=$TRAIN_FILE \
-    --do_train \
-    --beta 1.0 \
-    --dim_target_kl 1.0 \
-    --ratio_zero .5 \
-    --ratio_increase 0.25 \
-    --eval_data_file=$TEST_FILE \
-    --num_train_epochs 1.0 \
-    --save_steps 5000 \
-    --logging_steps 200 \
-    --overwrite_output_dir \
-    --per_gpu_train_batch_size 16 \
-    --block_size 256 \
-    --use_pretrained_model
+# CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_vae_training.py \
+#     --dataset Glue \
+#     --checkpoint_dir ../output/philly_scl_b16_g8_vae_wikipedia_pretraining_b0.0_d1.0_r01.0_ra0.1_200 \
+#     --gloabl_step_eval 880000 \
+#     --output_dir=../output/local_lm_vae_glue_bert_gpt \
+#     --encoder_model_type=bert \
+#     --encoder_model_name_or_path=bert-base-cased \
+#     --decoder_model_type=gpt2 \
+#     --decoder_model_name_or_path=gpt2 \
+#     --train_data_file=$TRAIN_FILE \
+#     --do_train \
+#     --beta 1.0 \
+#     --dim_target_kl 1.0 \
+#     --ratio_zero .5 \
+#     --ratio_increase 0.25 \
+#     --eval_data_file=$TEST_FILE \
+#     --num_train_epochs 1.0 \
+#     --save_steps 5000 \
+#     --logging_steps 200 \
+#     --overwrite_output_dir \
+#     --per_gpu_train_batch_size 16 \
+#     --block_size 256 \
+#     --use_pretrained_model
---
+++ code/scripts/scripts_local/run_lm_vae_bert_gpt_news.sh
@@ -0,0 +1,31 @@
+export PYTHONPATH="${PYTHONPATH}:/workspace/code"
+export GPU_ID=0,1
+
+
+export GPT2Model_PATH=/pretrain-models/gpt2-cn/gpt2-essay
+export BERTModel_PATH=/pretrain-models/bert/bert_torch_base_cn
+
+export TRAIN_FILE=/code/notebook/idea-gen/text_corpus/train.txt
+export TEST_FILE=/code/notebook/idea-gen/text_corpus/valid.txt
+
+
+CUDA_VISIBLE_DEVICES=$GPU_ID python examples/big_ae/run_lm_vae_training.py \
+    --dataset sgnews \
+    --output_dir=../output/local_lm_vae_sgnews_bert_gpt \
+    --encoder_model_type=bert \
+    --encoder_model_name_or_path=$BERTModel_PATH \
+    --decoder_model_type=gpt2 \
+    --decoder_model_name_or_path=$GPT2Model_PATH \
+    --train_data_file=$TRAIN_FILE \
+    --do_train \
+    --do_eval \
+    --beta 1.0 \
+    --ratio_zero .5 \
+    --ratio_increase 0.25 \
+    --eval_data_file=$TEST_FILE \
+    --num_train_epochs 5.0 \
+    --save_steps 2000 \
+    --logging_steps 100 \
+    --overwrite_output_dir \
+    --per_gpu_train_batch_size 8 \
+    --block_size 128
